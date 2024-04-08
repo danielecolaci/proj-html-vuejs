f08e5b02204fc18f8b66dc82bef0ef7e28ca78ea
danielecolaci <danielecolaci@outlook.com>
2024-04-08 21:37:39 +0200

    Set Header and Footer structure

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,7 +10,22 @@ export default {
 </script>
 
 <template>
-
+    <header>
+        <div class="debug container">
+            <nav class="debug flex">
+                <div class="debug logo"></div>
+                <div class="debug icon"></div>
+            </nav>
+            <div class=" debug jumbotron flex">
+                <section class="debug">
+                    <h1 class="serif">Barber Shop</h1>
+                    <hr>
+                    <p>The Pinnacle of Male Grooming</p>
+                    <button>Learn More</button>
+                </section>
+            </div>
+        </div>
+    </header>
 </template>
 
 <style scoped></style>
