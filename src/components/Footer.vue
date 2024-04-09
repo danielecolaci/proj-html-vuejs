41e014a537660066f541c71a789e4878c8758d8b
danielecolaci <danielecolaci@outlook.com>
2024-04-09 11:56:39 +0200

    Set layout structure in all sections

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,27 +10,29 @@ export default {
 </script>
 
 <template>
-<footer>
-    <div class="container debug">
-        <div class="nav debug flex">
-            <div class="product flex debug">
-                <div class="image debug"></div>
-                <button>Shop our range now</button>
+    <footer>
+        <div class="container debug">
+            <div class="nav debug flex">
+                <div class="flex debug column">
+                    <div class="image debug"></div>
+                    <button>Shop our range now</button>
+                </div>
+                <div class="contacts flex debug text-center column">
+                    <p>Avada Barbers<br>123 New York Street<br>New York City<br>info@yourwebsite.com<br>+1 (555)
+                        555-1212</p>
+                    <h3>Follow <i class="fa fa-user" aria-hidden="true"></i></h3>
+                </div>
+                <div class="flex debug column">
+                    <div class="image debug"></div>
+                    <button>Shop our range now</button>
+                </div>
             </div>
-            <div class="contacts flex debug">
-                <p>Avada Barbers<br>123 New York Street<br>New York City<br>info@yourwebsite.com<br>+1 (555) 555-1212</p>
-            <h3>Follow <i class="fa fa-user" aria-hidden="true"></i></h3>
-            </div>
-            <div class="product flex debug">
-                <div class="image debug"></div>
-                <button>Shop our range now</button>
+            <div class="copyright debug">
+                <p class="center">&copy; Copyright 2024 | Avada Theme by Daniele Colaci | All Rights Reserved | Powered
+                    by Boolean</p>
             </div>
         </div>
-        <div class="copyright debug">
-            <p class="center">&copy; Copyright 2024 | Avada Theme by Daniele Colaci | All Rights Reserved | Powered by Boolean</p>
-        </div>
-    </div>
-</footer>
+    </footer>
 </template>
 
 <style scoped></style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -10,7 +10,145 @@ export default {
 </script>
 
 <template>
+    <main>
+        <div class="pro debug">
+            <div class="center section-title">
+                <h4>Award Winning</h4>
+                <h1 class="serif">Pro Barbers</h1>
+                <button>Meet your new barber</button>
+            </div>
+        </div>
 
+        <div class="services debug">
+            <div class="container flex debug column">
+                <div class="center">
+                    <div class="section-title text-center">
+                        <h4>mens grooming</h4>
+                        <h1 class="serif">Services</h1>
+                    </div>
+                    <div class="flex">
+                        <section class="text-center">
+                            <h2 class="serif">Trim & Cut</h2>
+                            <p>Avada Barbers are expert in the lickety split trim and hair cut. Quick but careful and
+                                ridiculously good looking.</p>
+                        </section>
+                        <section class="text-center">
+                            <h2 class="serif">Wash & Dry</h2>
+                            <p>Take a seat in our fine leather chairs lean back and let us lather you a fresh head in a
+                                luxurious fashion.</p>
+                        </section>
+                        <section class="text-center">
+                            <h2 class="serif">Beard Tidy</h2>
+                            <p>Tame the tangles and untidy facial hairs like a gentleman with our Beard Tidy services
+                                from
+                                Avada Barbers.</p>
+                        </section>
+                    </div>
+                    <button>Read about our services</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="products debug">
+            <div class="flex column">
+                <div class="center">
+                    <div class="section-title text-center">
+                        <h4>we have you covered</h4>
+                        <h1 class="serif">Avada Grooming Products</h1>
+                    </div>
+                    <div class="flex product">
+                        <section class="text-center">
+                            <h2 class="serif">Brush</h2>
+                            <p>$15.00</p>
+                        </section>
+                        <section class="text-center">
+                            <h2 class="serif">Scissors</h2>
+                            <p>$85.00</p>
+                        </section>
+                        <section class="text-center">
+                            <h2 class="serif">Hot Oil</h2>
+                            <p>$15.00</p>
+                        </section>
+                        <section class="text-center">
+                            <h2 class="serif">Straight Razor</h2>
+                            <p>$30.00</p>
+                        </section>
+                    </div>
+                    <button>Shop our product range</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="reviews debug">
+            <div class="container flex debug column">
+                <div class="center">
+                    <div class="section-title text-center">
+                        <h4>what people say</h4>
+                        <h1 class="serif">Reviews</h1>
+                    </div>
+                    <div class="flex">
+                        <section class="text-center flex column flex-center">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo possimus vel provident
+                                eligendi
+                                quibusdam, itaque quaerat deserunt aut cumque quasi.</p>
+                            <div class="person debug"></div>
+                            <h5>John Doe</h5>
+                        </section>
+
+                        <section class="text-center flex column flex-center">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo possimus vel provident
+                                eligendi
+                                quibusdam, itaque quaerat deserunt aut cumque quasi.</p>
+                            <div class="person debug"></div>
+                            <h5>John Doe</h5>
+                        </section>
+
+                        <section class="text-center flex column flex-center">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo possimus vel provident
+                                eligendi
+                                quibusdam, itaque quaerat deserunt aut cumque quasi.</p>
+                            <div class="person debug"></div>
+                            <h5>John Doe</h5>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="blog debug">
+
+            <div class="container flex debug column">
+                <div class="center">
+                    <div class="section-title text-center">
+                        <h4>tales from the barber shop</h4>
+                        <h1 class="serif">Recent Blog Post</h1>
+                    </div>
+                    <div class="flex">
+                        <section class="text-center">
+                            <h2 class="serif">Avada Barbers Now Open</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita cumque at architecto
+                                qui,
+                                repellat optio.</p>
+                        </section>
+                        <section class="text-center">
+                            <h2 class="serif">Choosing The Right Barber</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita cumque at architecto
+                                qui,
+                                repellat optio.</p>
+                        </section>
+                        <section class="text-center">
+                            <h2 class="serif">Quick Service Guide</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita cumque at architecto
+                                qui,
+                                repellat optio.</p>
+                        </section>
+                    </div>
+                    <button>Read more from our blog</button>
+                </div>
+            </div>
+
+        </div>
+    </main>
 </template>
 
 <style scoped></style>
