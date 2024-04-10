1e95acd0046160d78bc9d8ed9341cbb7e3feeb9b
danielecolaci <danielecolaci@outlook.com>
2024-04-10 17:10:12 +0200

    Add Modal with js function to open and close

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,20 +3,39 @@ export default {
     name: 'Header',
     data() {
         return {
-
+            modal: false
+        }
+    },
+    methods: {
+        modalOpen() {
+            this.modal = !this.modal
+        },
+        modalClose() {
+            this.modal = false
         }
     }
 }
 </script>
 
 <template>
+    <div class="modale flex column flex-center" v-if="modal">
+        <h3 class="current">Home</h3>
+        <h3>About Us</h3>
+        <h3>Services</h3>
+        <h3>Shop</h3>
+        <h3>Our Team</h3>
+        <h3>Blog</h3>
+        <h3>Contact Us</h3>
+        <i class="fa-solid fa-xmark" @click="modalClose"></i>
+    </div>
+
     <header id="header">
         <div class="container">
             <nav class="flex">
                 <div class="logo image"></div>
                 <div class="icon flex">
                     <i class="fa-solid fa-cart-shopping"></i>
-                    <i class="fa-solid fa-bars"></i>
+                    <i class="fa-solid fa-bars" @click="modalOpen"></i>
                 </div>
             </nav>
             <div class="jumbotron flex image">
